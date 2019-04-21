61f472bcd35aa4afc36fd75c38cf7266e83ef70c
Ovidiu <ovidiu.b@overtel.com>
2019-04-21 15:48:53 +0200

    First commit

---
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.buttonclickapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener {
+            textView?.append(userInput.text)
+            textView?.append("\n")
+
+            userInput.text.clear()
+        }
+    }
+}
